2bf5e92114f70ede6d9f207f1707556611ebb1b5
mbutler <mbutler@cthulhu.intermediawritingsystems.com>
2011-03-30 13:39:31 -0500

    change class name

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <style type="text/css">
-	.clue { width: 300px; }
-	.clue label { float: left; width: 100px; }
+	.phone { width: 300px; }
+	.phone label { float: left; width: 100px; }
 </style>
 
 <p>Sign up for Ben Franklin's List of Synonyms for Drunkeness sent directly to your phone.  Every Friday night at 9pm CST you will be sent a random term from Franklin's big list via text message. Casually work it into your drunken conversations for that weekend and party like it's 1737. Impress your friends with archaic 18th century terms to describe your fucked up life!</p>
@@ -8,7 +8,7 @@
 <p>Enter your 10 digit phone number with no spaces or dashes.  If you get sick of getting these every Friday night just send me an email and I'll make it stop. --mbutler</p>
 
 <div class="example">
-	<form name="clue" method="POST" action="add.php">
+	<form name="phone" method="POST" action="add.php">
 		<label for="phoneNumber">Phone Number:</label><input type="text" maxlength="10" name="phoneNumber" id="phoneNumber" /><br />
 		<!--
         <label for="subject">Subject:</label><input type="text" name="subject" id="subject" /><br />
